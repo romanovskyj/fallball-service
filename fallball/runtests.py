c66d49ad0862de80cf5ee95d6f1af34acc61bfb4
eromanovskyj <romedik@gmail.com>
2016-06-23 17:11:22 +0300

    travis

--- fallball/runtests.py
+++ fallball/runtests.py
@@ -13,13 +13,14 @@ import sys
 print(sys.path)
 
 
+
 # As parent directory has also 'fallball' name it needs to specify the package explicitly:
 if __name__ is not '__main__':
     print("I am here")
     current_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     sys.path.insert(0, os.path.join(current_path, 'fallball'))
     reload(fallball)
-    
+
     print(fallball)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'fallball.settings'
